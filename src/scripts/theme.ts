6d356a61e44451d5d5c2fc8619bce889f15bfc1b
James <jyyoung@jyymail.com>
2025-01-31 17:57:54 -0500

    add search and tag filtering

--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -2,35 +2,37 @@ let toggle = document.getElementById("theme-toggle");
 const darkTheme = document.getElementById("dark-theme");
 const lightTheme = document.getElementById("light-theme");
 
-const storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+const storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light");
 console.log(storedTheme);
 
-if (storedTheme == "dark"){
-    document.documentElement.setAttribute('data-theme', storedTheme);
-    darkTheme!.style.display = "flex";
-    lightTheme!.style.display = "none";
+if (storedTheme == "dark") {
+  document.documentElement.setAttribute("data-theme", storedTheme);
+  darkTheme!.style.display = "flex";
+  lightTheme!.style.display = "none";
+} else {
+  document.documentElement.setAttribute("data-theme", storedTheme);
+  darkTheme!.style.display = "none";
+  lightTheme!.style.display = "flex";
 }
-else{
-    document.documentElement.setAttribute('data-theme', storedTheme);
+
+toggle!.onclick = (): void => {
+  let currentTheme: string | null =
+    document.documentElement.getAttribute("data-theme");
+  // console.log(currentTheme);
+  if (currentTheme === "dark") {
+    currentTheme = "light";
     darkTheme!.style.display = "none";
     lightTheme!.style.display = "flex";
-}
+  } else {
+    currentTheme = "dark";
+    darkTheme!.style.display = "flex";
+    lightTheme!.style.display = "none";
+  }
 
-toggle!.onclick = ():void => {
-    let currentTheme : string | null = document.documentElement.getAttribute("data-theme");
-    // console.log(currentTheme);
-    if (currentTheme === "dark"){
-        currentTheme = "light"
-        darkTheme!.style.display = "none";
-        lightTheme!.style.display = "flex";
-    }
-    else{
-        currentTheme = "dark"
-        darkTheme!.style.display = "flex";
-        lightTheme!.style.display = "none";
-    }
-
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    localStorage.setItem('theme', currentTheme);
-    
-}
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  localStorage.setItem("theme", currentTheme);
+};
